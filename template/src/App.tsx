c8743afc45fc77c72e1046c78458d8ba7c77f5e0
Daniel Silva <azsidaniel@gmail.com>
2021-02-28 22:19:04 -0300

    Inicia projeto e configura o path alias.

---
+++ template/src/App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>Olá, bem-vindo ao template mercúrio!</Text>
+    </View>
+  );
+};
+
+export default App;
